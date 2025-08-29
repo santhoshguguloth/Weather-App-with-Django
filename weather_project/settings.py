7951cd3a588a85c30f367ddaa08e64db7467a9e4
santhoshguguloth <santhoshgugloth6@gmail.com>
2025-08-29 21:48:53 +0530

    project-files

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'replace-this-with-a-secure-secret-in-production'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["weather-app-with-django-1.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
